d5ac00a4a9e6d042b35a6da550ad88ba6577b83b
nfwork <nfwork@163.com>
2019-11-23 10:57:50 +0800

    启动说明打印

--- src/main/java/com/nfwork/dbfound/springboot/demo/App.java
+++ src/main/java/com/nfwork/dbfound/springboot/demo/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
     	SpringApplication.run(App.class, args);
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello dbfound!" );
     }
 }
