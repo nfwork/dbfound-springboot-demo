b1b160c6ed58a6d0c52f17017ae407b348827fe1
nfwork <nfwork@163.com>
2019-11-23 15:42:30 +0800

    添加servlet启动类

---
+++ src/main/java/com/nfwork/dbfound/springboot/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nfwork.dbfound.springboot.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
+        return builder.sources(App.class);
+    }
+
+}
